c943cda2bc441ec30d601b62f5bf745f99d60684
Elyse Montano <elysemontano.dev.com>
2022-11-02 11:10:40 -0700

    Assessment review

--- code-challenges.js
+++ code-challenges.js
@@ -54,6 +54,42 @@ const temp2 = 350
 const temp3 = 212
 // Expected output: "212 is at boiling point"
 
+// Describe your process:
+    // Start by creating a function. I will name it temperature. Use proper arrow function syntax. const temperature = () => {}
+    // The function will take a single parameter of num1.
+    // The function code block will perform differently depending on what is input in place of num1. In order to make this happen, I will utilize a conditional statement. It will cover three cases.
+        // 1st case: num1 is less than 212. Return the string `${num1} is below boiling point`
+        // 2nd case: num1 is greater than 212. Return the string `${num1} is above boiling point`
+        // 3rd case: num1 is equal to 212. Return the string `${num1} is at boiling point`
+            // More specifically, I will be comparing num1 to 212 using relational operators for each of the three cases.
+            // Additionally, I will include a final else statement to act as a catch all in case the program doesn't like the input for num1.
+    // Once the function is properly coded, I will log it to the console by calling upon the temperature function with the given variables temp1/temp2/temp3 inside of the function parameter.
+    // Creation of the function below. The act of calling upon the function will be below each respective set.
+
+    const temperature = (num1) => {
+      if (num1 < 212) {
+          return `${num1} is below boiling point`
+      } else if (num1 > 212) {
+          return `${num1} is above boiling point`
+      } else if (num1 === 212) {
+          return `${num1} is at boiling point`
+      } else {
+          return "Oop, something went wrong."
+      }
+  }
+  
+
+  console.log(temperature(temp1))  // Actual output: "42 is below boiling point"
+  // Expected output: "42 is below boiling point"
+  
+
+  console.log(temperature(temp2))  // Actual output: "350 is above boiling point"
+  // Expected output: "350 is above boiling point"
+  
+
+  console.log(temperature(temp3))  // Actual output: "212 is at boiling point"
+  // Expected output: "212 is at boiling point"
+
 // --------------------2) Create the code that will combine the two arrays and return the length using the test variables provided below.
 
 // Describe your process:
@@ -62,6 +98,41 @@ const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
 const padres1998WorldSeriesRuns = [6, 3, 5, 3]
 // Expected output: 9
 
+// Describe your process:
+//No function is necessary though it is possible to create one that would work.
+//Use the built in concat method to combine the strings then use the built in length method to get the length. 
+//Need to create a new array variable because we are using an accessor method.
+//Also possible to concat with the spread operator like ...arr. This is also an accessor method as it will not change the original array.
+//bonus function that takes in two arrays, concats them, then returns their length
+
+//(Garrett)
+// Expected output: 9
+
+let padresCombined = padres1984WorldSeriesRuns.concat(padres1998WorldSeriesRuns)
+console.log(padresCombined.length); // 9
+
+let padsCombined = [...padres1984WorldSeriesRuns, ...padres1998WorldSeriesRuns]
+console.log(padsCombined.length); // 9
+
+let combine = (arr1, arr2) => {
+    let combined = [...arr1, ...arr2]
+    return combined.length
+}
+console.log(combine(padres1984WorldSeriesRuns, padres1998WorldSeriesRuns)); // 9
+
+
+//(Mike)
+
+// Describe your process:
+    //Pseudo code
+        //Combined two given arrays with built in method .concat and .length.
+        //Inputs: padres1984WorldSeriesRuns, & padres1998WorldSeriesRuns
+        //Output: Length of both arrays combined
+
+        // Expected output: 9
+var padresWorldSeriesRuns = padres1984WorldSeriesRuns.concat(padres1998WorldSeriesRuns)
+console.log(padresWorldSeriesRuns.length)
+
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below.
 
 // Describe your process:
@@ -69,23 +140,76 @@ const padres1998WorldSeriesRuns = [6, 3, 5, 3]
 const currentCohort = "Golf 2022"
 // Expected output: "2202 floG"
 
-// --------------------4) Create the code that will return the last index of the given value from the array using the test variables provided below.
 
 // Describe your process:
+    // input = Golf 2022
+    // output = 2022 floG
+    // create a variable called reverseString
+        // use accessor .split('') to split the string into an array of letters
+        // use accessor .reverse() to reverse the letters
+        // use accessor .join('') to join the letters back together and covert the array back into a string
 
+ 
+
+        // Expected output: "2202 floG"
+        
+        // const reverseString = currentCohort.split('').reverse().join('')
+        // console.log(reverseString) // "2202 floG"
+
+        // const reverseString = currentCohort.reverse()
+        // console.log(reverseString) // "2202 floG"
+        // TypeError: currentCohort.reverse is not a function
+ 
+
+
+console.log("--------------------4")
+// Create the code that will return the last index of the given value from the array using the test variables provided below.
+// Describe your process:
 const myNumbers = [13, 34, 42, 5, 5, 10, 27, 42, 10]
 const givenValue1 = 42
-// Expected output: 7
+// A function that takes an array and a value,
+// then find the last index of the given value from the array, using .lastindexOf,
+// then returns the value
+const findLastIndex = (arr1, val1) => {
+    let lastIndex = arr1.lastIndexOf(val1)
+    return(lastIndex)
+}
+console.log('Expected output: 7')
+console.log(`Student output: ${findLastIndex(myNumbers, givenValue1)}`)
 
 const givenValue2 = 10
-// Expected output: 8
+console.log("Expected output: 8")
+console.log(`Student output: ${findLastIndex(myNumbers, givenValue2)}`)
+
+
+
 
 // --------------------5) Create the code that will sort all the numbers in an array from largest to smallest. Use the test variables provided below.
 
 // Describe your process:
+//Input: use the variables as the argument
+    //each variable has an unsorted array of numbers
+//Process: Normally using .sort() is used for numbers sort in the array from smallest to largest with the argument inside the parentheses being (a, b) => a - b. The values a and b are used to compare two numbers, the comparison continues throughout the array
+// But if you want to sort the numbers from largest to smallest, you reverse the values by having the argument (a, b) => b - a
 
 const sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76]
 // Expected output: [82, 80, 79, 77, 76, 73, 72]
 
 const sanDiegoWinterTemperatures = [59, 68, 62, 59, 66, 67, 66]
 // Expected output: [68, 67, 66, 66, 62, 59, 59]
+
+const sortTemp = (temps) => {
+    return temps.sort((a, b) => b - a);
+}
+console.log(sortTemp(sanDiegoSummerTemperatures));
+console.log(sortTemp(sanDiegoWinterTemperatures));
+
+// Anonymous function
+// (a, b) => a - b
+
+// const newArray = [79, 100, 4, 80, 72, 73, 82, 77, 76, 200]
+// console.log(newArray.sort())
+// [
+//   100, 200,  4, 72, 73,
+//    76,  77, 79, 80, 82
+// ]
--- coding-concepts.js
+++ coding-concepts.js
@@ -15,18 +15,23 @@ console.log(colors.push("indigo"))
 // --------------------1) What will this log?
 
 const cohort = "LEARN 2022"
-// console.log(cohort.length)
+console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 10
+// b) Verify and explain: .length built-in method will return number of values in the string, including the space.
+
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
-// console.log(greeting[4])
+console.log(greeting[4])
+// a) Your answer: o
+// b) Verify and explain: The number four inside the brackets indicates the index position beginning with 0 within the const greeting. The index position returns the 4th indexed position starting with 0. h=0 e=1 l=2 l=3 o=4
 
 // a) Your answer:
+    // Answer: This should return the index of "o"
 // b) Verify and explain:
+    // Answer: The "o" is at the index of 4
 
 // --------------------3) What will this log?
 
@@ -34,21 +39,27 @@ const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
 // console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Ruby
+// b) Verify and explain: console.log(languages[]) will reference what is provided in the index which is in this case, 1.
+
+// // a) Your answer: Ruby
+// // b) Verify and explain: This function will return Ruby because of the 1 and [index]. This allows the first word in the array to be counted as a single index so starting at 0 we count 1 bringing us to Ruby.
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Code doesn't work
+// b) Verify and explain: toUpperCase built in method only works for strings, the const weekendDays is an array.
+
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean", "undefined"]
 // console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+
+// a) Your answer: a logical guess would be string 4
+
+// b) Verify and explain: typeof does return a kind of data type or object but what happened here is since datatypes.length turns out to equal the index number (4), typeof read the number 4 and said "oh thats a number so I'll output 'number'".
